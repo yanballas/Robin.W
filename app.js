ddbc4e3adef7739b8849b9d8e58588d7ad114dde
yanballas <yanballas@yandex.ru>
2023-07-23 14:32:47 +0300

    slide script, change header

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 window.addEventListener('DOMContentLoaded', () => {
 
+    let counter = 1;
+
     const fixedMenu = () => {
         const nav = document.querySelector('.hero-section-nav')
 
@@ -12,6 +14,22 @@ window.addEventListener('DOMContentLoaded', () => {
         })
     }
 
+    const headerSlider = document.querySelectorAll('.hero-section_content_slider_slide');
+
+    const slider = (array) => {
+        setInterval(() => {
+            array.forEach(element => {
+                element.classList.remove('_show')
+            });
+            array[counter].classList.add('_show');
+            counter++;
+            if (counter >= array.length) {
+                counter = 0;
+            }
+        }, 4000)
+    }
+
     fixedMenu();
+    slider(headerSlider);
 
 })
